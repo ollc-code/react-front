4958fbd94a94629ad5776aad329fa42cc986c437
San Luke Sequeira <42314542+sansqra@users.noreply.github.com>
2020-11-02 18:22:07 +0530

    Update index.js

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,3 @@
-import { Announcement } from '@material-ui/icons';
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/Navbar'
