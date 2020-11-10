77a8adc36179d909c9eadc489a2a15ced65f8982
keane3pereira <keane3pereira@gmail.com>
2020-11-10 22:26:35 +0530

    constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export var API_BASE_URL = "http://localhost:8000/";
-export var ENVIRONMENT = "development";
+export var API_BASE_URL = "https://orlem-connect-react.herokuapp.com/";
+export var ENVIRONMENT = "deploy-test";
